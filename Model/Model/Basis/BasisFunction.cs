144939a395d31f624debf550ab6233fb239341b6
Creray <lebed.yaroslav.edu@gmail.com>
2026-03-06 23:19:36 +0700

    feat: Added oriented basis function decorator

--- Model/Model/Basis/BasisFunction.cs
+++ Model/Model/Basis/BasisFunction.cs
@@ -7,3 +7,18 @@ public interface IBasisFunction
     double Value(Vector2D point);
     Vector2D Derivatives(Vector2D point);
 }
+
+public struct OrientedBasisFunction(IBasisFunction basis) : IBasisFunction
+{
+    private int _sign = 1;
+    private IBasisFunction _basis = basis;
+
+    public bool IsOrientationFlipped
+    {
+        readonly get => _sign == -1;
+        set => _sign = value ? -1 : 1;
+    }
+
+    public readonly double Value(Vector2D point) => _sign * _basis.Value(point);
+    public readonly Vector2D Derivatives(Vector2D point) => _sign * _basis.Derivatives(point);
+}
