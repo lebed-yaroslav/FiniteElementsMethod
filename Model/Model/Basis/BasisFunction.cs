b8c48a62d71a698c2a2e818fe0dfd8d10f53e308
Creray <lebed.yaroslav.edu@gmail.com>
2026-03-06 23:49:21 +0700

    fix: CS0445 related to OrientedBasisFunction

--- Model/Model/Basis/BasisFunction.cs
+++ Model/Model/Basis/BasisFunction.cs
@@ -8,17 +8,13 @@ public interface IBasisFunction
     Vector2D Derivatives(Vector2D point);
 }
 
-public struct OrientedBasisFunction(IBasisFunction basis) : IBasisFunction
+public sealed class OrientedBasisFunction(IBasisFunction basis) : IBasisFunction
 {
     private int _sign = 1;
-    private IBasisFunction _basis = basis;
+    private readonly IBasisFunction _basis = basis;
 
-    public bool IsOrientationFlipped
-    {
-        readonly get => _sign == -1;
-        set => _sign = value ? -1 : 1;
-    }
+    public bool IsOrientationFlipped { get => _sign == -1; set => _sign = value ? -1 : 1; }
 
-    public readonly double Value(Vector2D point) => _sign * _basis.Value(point);
-    public readonly Vector2D Derivatives(Vector2D point) => _sign * _basis.Derivatives(point);
+    public double Value(Vector2D point) => _sign * _basis.Value(point);
+    public Vector2D Derivatives(Vector2D point) => _sign * _basis.Derivatives(point);
 }
