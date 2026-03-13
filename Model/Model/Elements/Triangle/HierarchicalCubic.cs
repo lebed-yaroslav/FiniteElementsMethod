7b92c6aeb0f59637edb90a10e2540c301118d84a
Creray <lebed.yaroslav.edu@gmail.com>
2026-03-13 19:37:45 +0700

    cleanup: Formatting + `SetEdgeDof` implementation

--- Model/Model/Elements/Triangle/HierarchicalCubic.cs
+++ Model/Model/Elements/Triangle/HierarchicalCubic.cs
@@ -3,11 +3,12 @@ using Telma;
 
 namespace Model.Model.Elements.Triangle;
 
+
 public sealed class HierarchicalCubicTriangleFactory : IFiniteElementFactory
 {
     public IFiniteElement Create(IMesh2D mesh, int[] vertices, int materialIndex)
     {
-        var basis = (BasisSet)Basis;
+        var basis = (BasisSet)DefaultBasis();
         return new FiniteElement(
           Geometry: new TriangleGeometry(vertices) { Mesh = mesh },
           DOF: new Dof(basis),
@@ -16,54 +17,39 @@ public sealed class HierarchicalCubicTriangleFactory : IFiniteElementFactory
         );
     }
 
-    public static IBasisSet Basis => new BasisSet(
-      Quadratures.TriangleOrder6,
-      TriangleBasis.L3,
-      TriangleBasis.L1,
-      TriangleBasis.L2,
-      TriangleBasis.L3L1,
-      TriangleBasis.L1L2,
-      TriangleBasis.L2L3,
-      new OrientedBasisFunction(TriangleBasis.L1L3L1SubL3),
-      new OrientedBasisFunction(TriangleBasis.L1L2L2SubL1),
-      new OrientedBasisFunction(TriangleBasis.L2L3L2SubL3),
-      TriangleBasis.L1L2L3
+    public static IBasisSet DefaultBasis() => new BasisSet(
+        Quadratures.TriangleOrder6,
+        TriangleBasis.L3,
+        TriangleBasis.L1,
+        TriangleBasis.L2,
+        TriangleBasis.L3L1,
+        TriangleBasis.L1L2,
+        TriangleBasis.L2L3,
+        new OrientedBasisFunction(TriangleBasis.L1L3L1SubL3),
+        new OrientedBasisFunction(TriangleBasis.L1L2L2SubL1),
+        new OrientedBasisFunction(TriangleBasis.L2L3L2SubL3),
+        TriangleBasis.L1L2L3
     );
 
     public sealed class Dof(IBasisSet basis) : DofManager(dofCount: 10)
     {
-        private IBasisSet _basis = basis;
+        private readonly IBasisSet _basis = basis;
 
         public override int NumberOfDofOnVertex => 1;
         public override int NumberOfDofOnEdge => 2;
         public override int NumberOfDofOnElement => 1;
 
-        public override void SetEdgeDof(int localEdgeIndex, bool isOrientationFlipped, int n, int dofIndex)
-        {
-            _dof[3 + 3 * n + localEdgeIndex] = dofIndex;
-
-            if (n == 1 && isOrientationFlipped)
-            {
-                switch (localEdgeIndex)
-                {
-                    case 0:
-                        ((OrientedBasisFunction)_basis.Basis[6]).IsOrientationFlipped = true;
-                        break;
-                    case 1:
-                        ((OrientedBasisFunction)_basis.Basis[7]).IsOrientationFlipped = true;
-                        break;
-                    case 2:
-                        ((OrientedBasisFunction)_basis.Basis[8]).IsOrientationFlipped = true;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
         public override void SetVertexDof(int localVertexIndex, int n, int dofIndex)
         {
             _dof[localVertexIndex] = dofIndex;
+        }
+
+        public override void SetEdgeDof(int localEdgeIndex, bool isOrientationFlipped, int n, int dofIndex)
+        {
+            int basisIndex = 3 + 3 * n + localEdgeIndex;
+            _dof[basisIndex] = dofIndex;
+            if (n == 1)
+                ((OrientedBasisFunction)_basis.Basis[basisIndex]).IsOrientationFlipped = isOrientationFlipped;
         }
 
         public override void SetElementDof(int n, int dofIndex)
