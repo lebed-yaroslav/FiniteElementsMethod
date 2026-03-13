ba5613e975f22cdff392fe7e4fbeb63f14a0d743
Creray <lebed.yaroslav.edu@gmail.com>
2026-03-13 21:36:07 +0700

    cleanup: Remove unused imports

--- Model/Core/CoordinateSystem/CoordinateTransform.cs
+++ Model/Core/CoordinateSystem/CoordinateTransform.cs
@@ -1,8 +1,7 @@
-﻿using System.Diagnostics;
-using Telma;
-using Telma.Extensions;
+﻿using Telma.Extensions;
 
 namespace Model.Core.CoordinateSystem;
+
 
 public interface ICoordinateTransform<TSource, TTarget>
     where TSource : IVectorBase<TSource>
