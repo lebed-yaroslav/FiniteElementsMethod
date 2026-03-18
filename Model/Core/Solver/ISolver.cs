84802aad6a8ce05468937c234463f60b096982cd
Creray <lebed.yaroslav.edu@gmail.com>
2026-03-19 00:38:00 +0700

    feat: Add basic solver interface

--- Model/Core/Solver/ISolver.cs
+++ Model/Core/Solver/ISolver.cs
@@ -1,5 +1,14 @@
 namespace Model.Core.Solver;
 
-internal interface ISolver
+
+public interface ISolver
 {
+    public record struct SolverParams(
+        double Eps = 1e-10,
+        int MaxIterations = 1000
+    );
+
+    (double residual, int iterations) Solve(ReadOnlySpan<double> rhsVector, Span<double> solution, SolverParams paramz);
+    (double residual, int iterations) Solve(ReadOnlySpan<double> rhsVector, Span<double> solution)
+        => Solve(rhsVector, solution, new());
 }
