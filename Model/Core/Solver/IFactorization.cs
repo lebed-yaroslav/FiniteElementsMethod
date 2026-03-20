53707400b77f70fecdb1eee8773006535b0042c8
stald10 <70633097+stald10@users.noreply.github.com>
2026-03-20 22:31:50 +0700

    Добавил МСГ

---
+++ Model/Core/Solver/IFactorization.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Model.Core.Matrix;
+
+namespace Model.Core.Solver
+{
+
+    public interface IFactorizationType : IMatrix 
+    {
+
+        void LMult(ReadOnlySpan<double> to, Span<double> res);
+        void UMult(ReadOnlySpan<double> to, Span<double> res);
+
+        public ReadOnlySpan<int> Ig { get; }
+        public ReadOnlySpan<int> Jg { get; }
+        public Span<double> Ggl { get; }
+        public Span<double> Di { get; }
+    }
+    public interface IFactorizationPrediconditioner 
+    {
+        IFactorizationType CreateFactor(CsrMatrix sparseMatrix, double eps);
+    }
+}
