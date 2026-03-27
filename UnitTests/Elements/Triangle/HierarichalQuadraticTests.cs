12ece8ad86e371582d0d867154da0aad303c7f83
Inazuma009 <wasisdas17@gmail.com>
2026-03-28 02:24:27 +0900

    add unit-tests for stiffness (hierarical quadratic)

--- UnitTests/Elements/Triangle/HierarichalQuadraticTests.cs
+++ UnitTests/Elements/Triangle/HierarichalQuadraticTests.cs
@@ -58,7 +58,54 @@ public class HierarichalQuadraticTests
 
         return mesh;
     }
+    [Fact]
+    public void NonMasterLocalStiffnessMatrix_IsSymmetric()
+    {
+        var mesh = BuildNonMasterUnitMesh();
+        foreach (var element in mesh.FiniteElements)
+        {
+            var G = NumericIntegrator.CalculateLocalStiffness(element, _ => 1.0);
+            for (int i = 0; i < G.Size; i++)
+            {
+                for (int j = 0; j < G.Size; j++)
+                    Assert.Equal(G[i, j], G[j, i], 1e-13);
+            }
+        }
+    }
+    [Fact]
+    public void NonMasterLocalStiffnessMultipliedByConstantIsZero()
+    {
+        var mesh = BuildNonMasterUnitMesh();
 
+        double[] u_expected = [3, 3, 3, 0, 0, 0]; //u=3
+        foreach (var element in mesh.FiniteElements)
+        {
+            var G = NumericIntegrator.CalculateLocalStiffness(element, _ => 1.0);
+            var result = new double[G.Size];
+            for (int i = 0; i < G.Size; i++)
+            {
+                for (int j = 0; j < G.Size; j++)
+                {
+                    result[i] += G[i, j] * u_expected[i];
+                }
+
+            }
+            for (int i = 0; i < result.Length; i++)
+                Assert.Equal(0.0, result[i], 1e-13);
+        }
+    }
+    private static Mesh2D BuildNonMasterUnitMesh()
+    {
+        var mesh = new Mesh2D(IdentityTransform<Vector2D>.Instance);
+
+        mesh.AddVertex(new Vector2D(0.0, 0.0)); // 0
+        mesh.AddVertex(new Vector2D(1.0, 2.0)); // 1
+        mesh.AddVertex(new Vector2D(3.0, 1.0)); // 2
+
+        mesh.AddElement(FiniteElements.Triangle.HierarchicalQuadratic, vertices: [0, 1, 2], materialIndex: 0);
+
+        return mesh;
+    }
 }
 
 
