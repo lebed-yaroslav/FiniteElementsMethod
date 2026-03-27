a2aba58475347eb45055057c6b93604a87e987ac
Creray <lebed.yaroslav.edu@gmail.com>
2026-03-27 17:32:33 +0700

    fix: Element extensions

--- Model/Model/Elements/ElementExtensions.cs
+++ Model/Model/Elements/ElementExtensions.cs
@@ -9,17 +9,18 @@ namespace Model.Model.Elements;
 
 public static class FiniteElementExtensions
 {
-    extension<TSpace>(IFiniteElement<TSpace> self)
+    extension<TSpace, TBoundary>(IFiniteElementBase<TSpace, TBoundary> self)
         where TSpace : IVectorBase<TSpace>
+        where TBoundary : IVectorBase<TBoundary>
     {
         public IMesh<TSpace> Mesh => self.Geometry.Mesh;
         public ReadOnlySpan<int> Vertices => self.Geometry.Vertices;
         public IEnumerable<Edge> Edges => self.Geometry.Edges;
         public int EdgeCount => self.Geometry.EdgeCount;
-        public ICoordinateTransform<TSpace, TSpace> MasterElementCoordinateSystem => self.Geometry.MasterElementCoordinateSystem;
+        public ICoordinateTransform<TSpace, TBoundary> MasterElementCoordinateSystem => self.Geometry.MasterElementCoordinateSystem;
 
-        public IEnumerable<Quadratures.Node<TSpace>> Quadratures => self.BasisSet.Quadratures;
-        public ReadOnlySpan<IBasisFunction<TSpace>> Basis => self.BasisSet.Basis;
+        public IEnumerable<Quadratures.Node<TBoundary>> Quadratures => self.BasisSet.Quadratures;
+        public ReadOnlySpan<IBasisFunction<TBoundary>> Basis => self.BasisSet.Basis;
     }
 
     extension<TSpace, TBoundary>(IBoundaryElement<TSpace, TBoundary> self)
