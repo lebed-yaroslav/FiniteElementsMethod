7377c2572b20d14a37cea709ae171d24c30981e4
Creray <lebed.yaroslav.edu@gmail.com>
2026-03-27 20:01:14 +0700

    draft: Initial Assembler implementation

--- Model/Model/Assembly/DofManager.cs
+++ Model/Model/Assembly/DofManager.cs
@@ -4,6 +4,27 @@ using Telma.Extensions;
 
 namespace Model.Model.Assembly;
 
+
+public sealed class DofManager
+{
+    public int TotalDofCount { get; private init; }
+    public int FreeDofCount { get; private init; }
+    public int FixedDofCount => TotalDofCount - FreeDofCount;
+
+    public static DofManager NumerateDof<TSpace, TBoundary>(
+        IMeshWithBoundaries<TSpace, TBoundary> mesh,
+        BoundaryCondition<TSpace>[] boundaryConditions
+    )
+        where TSpace : IVectorBase<TSpace>
+        where TBoundary : IVectorBase<TBoundary>
+    {
+        int totalDofCount = DofNumerator<TSpace, TBoundary>.PreNumerateDof(mesh);
+        int freeDofCount = DofNumerator<TSpace, TBoundary>.RenumberFixedDof(mesh, boundaryConditions, totalDofCount);
+
+        return new() { TotalDofCount = totalDofCount, FreeDofCount = freeDofCount };
+    }
+}
+
 public static partial class DofNumerator<TSpace, TBoundary>
     where TSpace : IVectorBase<TSpace>
     where TBoundary : IVectorBase<TBoundary>
@@ -12,7 +33,7 @@ public static partial class DofNumerator<TSpace, TBoundary>
     /// Производит первичную нумерацию степеней свободы
     /// </summary>
     /// <returns>Общее количество степеней свободы</returns>
-    public static int NumerateDof(IMeshWithBoundaries<TSpace, TBoundary> mesh)
+    public static int PreNumerateDof(IMeshWithBoundaries<TSpace, TBoundary> mesh)
     {
         int dofCount = 0;
 
@@ -84,11 +105,63 @@ public static partial class DofNumerator<TSpace, TBoundary>
         }
     }
 
-
     private static void NumerateElementDof(IMeshWithBoundaries<TSpace, TBoundary> mesh, ref int dofCount)
     {
         foreach (var element in mesh.AllElements)
             for (int i = 0; i < element.DOF.NumberOfDofOnElement; i++)
                 element.DOF.SetElementDof(i, dofCount++);
+    }
+
+    /// <summary>
+    /// Производит перенумерацию степеней свободы таким образом,
+    /// что все зафиксированные условиями Дирихле узлы оказываются в конце
+    /// </summary>
+    /// <param name="totalDofCount">Общее количество степеней свободы</param>
+    /// <returns>Количество свободных узлов</returns>
+    public static int RenumberFixedDof(
+        IMeshWithBoundaries<TSpace, TBoundary> mesh,
+        BoundaryCondition<TSpace>[] boundaryConditions,
+        int totalDofCount
+    ) => RenumberFixedDof(
+        fixedElementsDof: mesh.FixedElementsBy(boundaryConditions).Select(e => e.DOF),
+        elementsDof: mesh.AllElementsDof,
+        totalDofCount: totalDofCount
+    );
+
+    /// <summary>
+    /// Производит перенумерацию степеней свободы таким образом,
+    /// что все зафиксированные условиями Дирихле узлы оказываются в конце
+    /// </summary>
+    /// <param name="fixedElementsDof">Степени свободы граничных элементов, на которых заданы условия Дирихле</param>
+    /// <param name="elementsDof">Степени свободы всех элементов сетки</param>
+    /// <param name="totalDofCount">Общее количество степеней свободы</param>
+    /// <returns>Количество свободных узлов</returns>
+    public static int RenumberFixedDof(
+        IEnumerable<IDofManager> fixedElementsDof,
+        IEnumerable<IDofManager> elementsDof,
+        int totalDofCount
+    )
+    {
+        var fixedDof = new HashSet<int>();
+        foreach (var elementDof in fixedElementsDof)
+        {
+            foreach (int dof in elementDof.Dof)
+                fixedDof.Add(dof);
+        }
+
+        int start = 0;
+        int finish = totalDofCount - 1;
+        int[] numberMapping = new int[totalDofCount];
+
+        for (int i = 0; i < totalDofCount; i++)
+            if (fixedDof.Contains(i))
+                numberMapping[i] = finish--;
+            else
+                numberMapping[i] = start++;
+
+        foreach (var elementDof in elementsDof)
+            elementDof.Renumber(i => numberMapping[i]);
+
+        return start;
     }
 }
