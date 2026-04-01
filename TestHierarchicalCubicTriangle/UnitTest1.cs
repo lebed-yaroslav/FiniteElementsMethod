de3c593995e257937d4d02bf167fdcb98bddbb1d
stald10 <70633097+stald10@users.noreply.github.com>
2026-04-01 13:49:17 +0700

    Попытка инициализации с файла

---
+++ TestHierarchicalCubicTriangle/UnitTest1.cs
@@ -0,0 +1,82 @@
+using System.Security.Cryptography.X509Certificates;
+using Model.Core.CoordinateSystem;
+using Telma;
+
+
+namespace TestHierarchicalCubicTriangle
+{
+    public class Test
+    {
+        [Fact]
+
+        p
+
+        public void Hierarchical() 
+        {
+           
+        }
+
+
+
+        public void HierarchicalTriangleThirdDegreeBasisFanction()
+        {
+            //ICoordinateSystem2D system2D = new CartesianCoordinateSystem();
+
+            //Func<ReadOnlySpan<double>, double> lambda = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> lambda1 = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> gamma = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> gamma2 = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> function = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> function1 = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> beta = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> beta1 = (ReadOnlySpan<double> coords) => 1;
+
+            //var problem = new Problem(lambda, gamma, "RowColSparseMatrix", system2D);
+            //var mesh2d = (Mesh2D)problem.mesh;
+
+
+            //Func<ReadOnlySpan<double>, double> Beta = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> UBeta = (ReadOnlySpan<double> coords) => 1;
+            //Func<ReadOnlySpan<double>, double> Theta = (ReadOnlySpan<double> coords) => 1;
+
+            //Func<ReadOnlySpan<double>, double> краевое = (ReadOnlySpan<double> coords) => 5;
+
+            //problem.InitializeProblem<LagrangeTriangleThirdDegree, BoundaryLagrangeTriangleThirdDegree>(краевое, @"C:\Users\02112\source\repos\finite-elements-method\UnitTest\test2.txt");
+            //problem.SetRightHandSide(function);
+            //problem.SetSecondBoundaryData(Theta);
+            //problem.SetThirdBoundaryData(Beta, UBeta);
+            //problem.AssembleVolumePart(function);
+            //problem.AssembleBoundaryPart(Beta, UBeta);
+
+            //var res = new double[10, 16];
+
+
+
+            ////foreach (var elem in mesh2d.elements)
+            ////{
+            //var elem = mesh2d.elements[0];
+            //for (int i = 0; i < ((IFiniteElementWithNumericIntegration)elem).Basis.Length; ++i)
+            //{
+            //    res[i, 0] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([0, 0]);
+            //    res[i, 1] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([1, 0]);
+            //    res[i, 2] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([0, 1]);
+            //    res[i, 3] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([1.0 / 3, 0]);
+            //    res[i, 4] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([2.0 / 3, 0]);
+            //    res[i, 5] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([2.0 / 3, 1.0 / 3]);
+
+            //    res[i, 6] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([1.0 / 3, 2.0 / 3]);
+            //    res[i, 7] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([0, 1.0 / 3.0]);
+            //    res[i, 8] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([0, 2.0 / 3]);
+            //    res[i, 9] = ((IFiniteElementWithNumericIntegration)elem).Basis[i].Value([1.0 / 3.0, 1.0 / 3.0]);
+            //}
+
+            //for (int i = 0; i < 10; ++i)
+            //    for (int j = 0; j < 10; ++j)
+            //    {
+            //        Console.WriteLine($"i = {i}, j = {j}");
+            //        Assert.Equal(res[i, j], (i == j) ? 1 : 0, 1e-14);
+            //    }
+
+        }
+    }
+}
---
+++ UnitTests/Elements/Triangle/HierarchicalTriangle.cs
@@ -0,0 +1,21 @@
+
+using Model.Core.CoordinateSystem;
+using Model.Model.Elements.Segment;
+using Model.Model.Elements.Triangle;
+using Model.Model.Mesh;
+using Telma;
+
+namespace UnitTests.Elements.Triangle
+{
+    public class HierarchicalTriangle
+    {
+        [Fact]
+        public void Hierrarchical() 
+        {
+            string path = @"C:\Users\Lossky\Documents\GitHub\FiniteElementsMethod\test.txt";
+
+            var init = MeshInput.ReadMesh(File.OpenText(path), PolarCoordinateSystem.Instance,new HierarchicalCubicTriangleFactory(),new HierarchicalQuadraticSegmentFactory());
+           
+        }
+    }
+}
