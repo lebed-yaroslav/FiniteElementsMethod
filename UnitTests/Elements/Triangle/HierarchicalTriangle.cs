059130f70b5f0ee9b733eb6702ec0740a90ec687
stald10 <70633097+stald10@users.noreply.github.com>
2026-04-02 01:20:46 +0700

    Отладка теста
    Исправили ошибку в генераторе списка смежности
    Unit Test содержит в себе следующее:
    1)Проверка корректности работы нумератора и перенумератора
    2)Проверка корректности построения списка смежности для глобальной матрицы
    3)Проваерка корректности построения списка смежности для матрицы проецирования весов (коэффициентов сплайна)
    4)Проверка генерации матрицы по списку смежности
    
    Co-Authored-By: Egor <130754485+OmniSSeRiTTiBi@users.noreply.github.com>

--- UnitTests/Elements/Triangle/HierarchicalTriangle.cs
+++ UnitTests/Elements/Triangle/HierarchicalTriangle.cs
@@ -1,4 +1,5 @@
 
+using System.Diagnostics;
 using Model.Core.CoordinateSystem;
 using Model.Core.Matrix;
 using Model.Model;
@@ -30,14 +31,57 @@ namespace UnitTests.Elements.Triangle
 
             //Нумератор + Перенумератор работает корректно
             int DOFcount = DofNumerator<Vector2D, Vector1D>.NumerateDof(init);
+
+            Assert.Equal(DOFcount, 12);
+
             int FreeDOFCount = DofNumerator<Vector2D, Vector1D>.RenumberFixedDof(init, boundaryCondition, DOFcount);
 
+            Assert.Equal(FreeDOFCount, 6);
 
-            //Списки смежности не собираются, выход за пределы
+            List<HashSet<int>> trueAdjacency = new();
+            trueAdjacency.Add([]);
+            trueAdjacency.Add([0]);
+            trueAdjacency.Add([0,1]);
+            trueAdjacency.Add([0,1]);
+            trueAdjacency.Add([0,3,1]);
+            trueAdjacency.Add([0,4]);
+
+            //Списки смежности не собираются, выход за пределы  PS: Исправлено
             var adjacency = PortraitGenerator.CreateAdjacencyList(init.AllElementsDof,0,FreeDOFCount);
-            var adjacencyDirichlet = PortraitGenerator.CreateAdjacencyList(init.AllElementsDof, DOFcount, FreeDOFCount);
+
+            Assert.Equal(adjacency, trueAdjacency);
+
+            List<HashSet<int>> trueAdjacencyDirichlet = new();
+            trueAdjacencyDirichlet.Add([]);
+            trueAdjacencyDirichlet.Add([]);
+            trueAdjacencyDirichlet.Add([1]);
+            trueAdjacencyDirichlet.Add([0]);
+            trueAdjacencyDirichlet.Add([3,0]);
+            trueAdjacencyDirichlet.Add([2, 1]);
+
+            //Лишний элемент в портрете
+            var adjacencyDirichlet = PortraitGenerator.CreateAdjacencyList(init.BoundaryElements
+            .Where(e => boundaryCondition[e.BoundaryIndex] is BoundaryCondition<Vector2D>.Dirichlet)
+            .Select(e => e.DOF), FreeDOFCount, DOFcount);
+            Assert.Equal(adjacencyDirichlet, trueAdjacencyDirichlet);
+
+
             var matrix = CsrMatrix.Portrait.Create(adjacency);
+
+            int[] trueMatrixIG = [0,0,1,3,5,8,10];
+            int[] trueMatrixJG = [0,0,1,0,1,0,1,3,0,4];
+
+            Assert.Equal(matrix.Ig, trueMatrixIG);
+            Assert.Equal(matrix.Jg, trueMatrixJG);
+            
             var matrixDirichlet = CsrMatrix.Portrait.Create(adjacencyDirichlet);
+
+            int[] trueMatrixDirichletIG = [0,0,0,1,2,4,6];
+            int[] trueMatrixDirichletJG = [1,0,0,3,1,2];
+
+            Assert.Equal(matrixDirichlet.Ig, trueMatrixDirichletIG);
+            Assert.Equal(matrixDirichlet.Jg, trueMatrixDirichletJG);
+
         }
     }
 }
