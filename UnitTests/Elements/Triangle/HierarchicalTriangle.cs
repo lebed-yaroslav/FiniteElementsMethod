ff38f0a0ada86b8771a8cc3e65585cfc46a64fe9
stald10 <70633097+stald10@users.noreply.github.com>
2026-04-01 22:06:43 +0700

    Тест инициализации
    Bug report: На Иерархическом Кубическом базисе не работает NumerateEdge. localEdgeIndex = 3 и не проходит в этот момент Debug.Assert
    
    Co-Authored-By: Egor <130754485+OmniSSeRiTTiBi@users.noreply.github.com>

--- UnitTests/Elements/Triangle/HierarchicalTriangle.cs
+++ UnitTests/Elements/Triangle/HierarchicalTriangle.cs
@@ -1,5 +1,10 @@
 
 using Model.Core.CoordinateSystem;
+using Model.Core.Matrix;
+using Model.Model;
+using Model.Model.Assembly;
+using Model.Model.Basis;
+using Model.Model.Elements;
 using Model.Model.Elements.Segment;
 using Model.Model.Elements.Triangle;
 using Model.Model.Mesh;
@@ -14,8 +19,23 @@ namespace UnitTests.Elements.Triangle
         {
             string path = @"C:\Users\Lossky\Documents\GitHub\FiniteElementsMethod\test.txt";
 
-            var init = MeshInput.ReadMesh(File.OpenText(path), PolarCoordinateSystem.Instance,new HierarchicalCubicTriangleFactory(),new HierarchicalQuadraticSegmentFactory());
-           
+            Mesh2D init = MeshInput.ReadMesh(File.OpenText(path), PolarCoordinateSystem.Instance,new HierarchicalQuadraticTriangleFactory(),new HierarchicalQuadraticSegmentFactory());
+            var a = init.BoundaryElements;
+
+            BoundaryCondition<Vector2D>[] boundaryCondition = new BoundaryCondition<Vector2D>[2];
+            Func<Vector2D, double, double> x = (y,d) => y.X;
+
+            boundaryCondition[0] = new BoundaryCondition<Vector2D>.Dirichlet(x);
+            boundaryCondition[1] = new BoundaryCondition<Vector2D>.Dirichlet(x);
+
+
+            int DOFcount = DofNumerator<Vector2D, Vector1D>.NumerateDof(init);
+            int FreeDOFCount = DofNumerator<Vector2D, Vector1D>.RenumberFixedDof(init, boundaryCondition, DOFcount);
+
+            var adjacency = PortraitGenerator.CreateAdjacencyList(init.AllElementsDof,0,FreeDOFCount);
+            var adjacencyDirichlet = PortraitGenerator.CreateAdjacencyList(init.AllElementsDof, DOFcount, FreeDOFCount);
+            var matrix = CsrMatrix.Portrait.Create(adjacency);
+            var matrixDirichlet = CsrMatrix.Portrait.Create(adjacencyDirichlet);
         }
     }
 }
