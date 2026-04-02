cffea63c6c4740b2765309ec2dd24807826b53b3
stald10 <70633097+stald10@users.noreply.github.com>
2026-04-02 23:13:11 +0700

    Тестирование Матрицы массы/жёсткости
    Bug report:
    В ReadMesh некорректный срез у списка для краевых условий
    CalculateStiffnes так-же считается некорректно

--- UnitTests/Elements/Triangle/HierarchicalTriangle.cs
+++ UnitTests/Elements/Triangle/HierarchicalTriangle.cs
@@ -5,11 +5,14 @@ using Model.Core.Matrix;
 using Model.Model;
 using Model.Model.Assembly;
 using Model.Model.Basis;
-using Model.Model.Elements;
+//using Model.Model.Elements;
 using Model.Model.Elements.Segment;
 using Model.Model.Elements.Triangle;
+using Model.Model.Integrator;
 using Model.Model.Mesh;
+using Model.Model.Problem;
 using Telma;
+using static Model.Core.CoordinateSystem.MatrixOperations;
 
 namespace UnitTests.Elements.Triangle
 {
@@ -30,13 +33,63 @@ namespace UnitTests.Elements.Triangle
             boundaryCondition[1] = new BoundaryCondition<Vector2D>.Dirichlet(x);
 
             //Нумератор + Перенумератор работает корректно
-            int DOFcount = DofNumerator<Vector2D, Vector1D>.NumerateDof(init);
+            int DOFcount = DofNumerator<Vector2D, Vector1D>.PreNumerateDof(init);
 
-            Assert.Equal(DOFcount, 12);
+            NumericItegrator2D Integrator = NumericIntegrator<Vector2D, Vector1D, Ops2X2>.Instance;
 
-            int FreeDOFCount = DofNumerator<Vector2D, Vector1D>.RenumberFixedDof(init, boundaryCondition, DOFcount);
+            var element = init.FiniteElements.First();
+            System.Func<Telma.Vector2D, double> lambda = x => 1;
+            var stiffness = Integrator.CalculateLocalStiffness(element, lambda);
+            var mass = Integrator.CalculateLocalMass(element, lambda);
 
-            Assert.Equal(FreeDOFCount, 6);
+
+            double[,] trueMass = new double[6, 6]
+{
+                {0.40000000000000169,0.19999999999999993,0.23333333333333361,0.077777777777777918,0.044444444444444189,0.088888888888889184 } ,
+                {0.19999999999999993,0.40000000000000108,0.23333333333333331,0.077777777777777848,0.088888888888889,0.044444444444444189 } ,
+                {0.23333333333333361,0.23333333333333331,0.53333333333333532,0.044444444444444189,0.10000000000000012,0.10000000000000024} ,
+                {0.077777777777777918,0.077777777777777848,0.044444444444444189,0.02539682539682549,0.014285714285714195,0.014285714285714221} ,
+                {0.044444444444444189,0.088888888888889,0.10000000000000012,0.014285714285714195,0.031746031746031841,0.015873015873015778} ,
+                {0.088888888888889184,0.044444444444444189,0.10000000000000024,0.014285714285714221,0.015873015873015778,0.031746031746031911}
+};
+            for (int i = 0; i < 6; ++i)
+            {
+                for (int j = 0; j < 6; ++j)
+                {
+                    Assert.Equal(trueMass[i, j], mass[i, j], 1e-12);
+                }
+            }
+
+
+            double[,] trueStiffnes = new double[6, 6] 
+            {
+                {2.8333333333333384,-0.16666666666666696,-2.6666666666666714,0.83333333333333437,-0.89583333333333448,0.22916666666666696 } ,
+                {-0.16666666666666696,0.16666666666666696,0,5.8980598183211441E-17,0.062500000000000139,-0.062500000000000139 } ,
+                {-2.6666666666666714,0,2.6666666666666714,-0.83333333333333448,0.83333333333333448,-0.16666666666666693} ,
+                {0.83333333333333437,5.8980598183211441E-17,-0.83333333333333448,0.42500000000000132,-0.40000000000000108,0.033333333333333326 } ,
+                {-0.89583333333333448,0.062500000000000139,0.83333333333333448,-0.40000000000000108,0.43333333333333451,-0.0666666666666668 } ,
+                {0.22916666666666696,-0.062500000000000139,-0.16666666666666693,0.033333333333333326,-0.0666666666666668,0.50000000000000322 }
+            };
+
+            for (int i = 0; i < 6; ++i) 
+            {
+                for (int j = 0; j < 6; ++j) 
+                {
+                    Assert.Equal(trueStiffnes[i, j], stiffness[i,j],1e-12);
+                }
+            }
+
+
+
+
+
+            var DOF = DofManager.NumerateDof(init,boundaryCondition);
+ 
+            Assert.Equal(DOF.TotalDofCount, 12);
+
+            //int FreeDOFCount = DofNumerator<Vector2D, Vector1D>.RenumberFixedDof(init, boundaryCondition, DOFcount);
+
+            Assert.Equal(DOF.FreeDofCount, 6);
 
             List<HashSet<int>> trueAdjacency = new();
             trueAdjacency.Add([]);
@@ -47,7 +100,7 @@ namespace UnitTests.Elements.Triangle
             trueAdjacency.Add([0,4]);
 
             //Списки смежности не собираются, выход за пределы  PS: Исправлено
-            var adjacency = PortraitGenerator.CreateAdjacencyList(init.AllElementsDof,0,FreeDOFCount);
+            var adjacency = PortraitGenerator.CreateAdjacencyList(init.AllElementsDof,0, DOF.FreeDofCount);
 
             Assert.Equal(adjacency, trueAdjacency);
 
@@ -62,11 +115,13 @@ namespace UnitTests.Elements.Triangle
             //Лишний элемент в портрете
             var adjacencyDirichlet = PortraitGenerator.CreateAdjacencyList(init.BoundaryElements
             .Where(e => boundaryCondition[e.BoundaryIndex] is BoundaryCondition<Vector2D>.Dirichlet)
-            .Select(e => e.DOF), FreeDOFCount, DOFcount);
+            .Select(e => e.DOF), DOF.FreeDofCount, DOF.TotalDofCount);
             Assert.Equal(adjacencyDirichlet, trueAdjacencyDirichlet);
 
+            var matrix = (CsrMatrix)(new CsrMatrixFactory().Create(adjacency));
 
-            var matrix = CsrMatrix.Portrait.Create(adjacency);
+      
+            //var matrix = CsrMatrix.Portrait.Create(adjacency);
 
             int[] trueMatrixIG = [0,0,1,3,5,8,10];
             int[] trueMatrixJG = [0,0,1,0,1,0,1,3,0,4];
@@ -74,13 +129,17 @@ namespace UnitTests.Elements.Triangle
             Assert.Equal(matrix.Ig, trueMatrixIG);
             Assert.Equal(matrix.Jg, trueMatrixJG);
             
-            var matrixDirichlet = CsrMatrix.Portrait.Create(adjacencyDirichlet);
+            var matrixDirichlet = (CsrMatrix)(new CsrMatrixFactory().Create(adjacencyDirichlet));
 
             int[] trueMatrixDirichletIG = [0,0,0,1,2,4,6];
             int[] trueMatrixDirichletJG = [1,0,0,3,1,2];
 
             Assert.Equal(matrixDirichlet.Ig, trueMatrixDirichletIG);
             Assert.Equal(matrixDirichlet.Jg, trueMatrixDirichletJG);
+
+
+
+
 
         }
     }
