ec15dc86f73aa47021070a54b3489bbcfea8ef1e
Creray <lebed.yaroslav.edu@gmail.com>
2026-04-03 12:22:17 +0700

    fix: Rebase conflicts

--- UnitTests/Core/Solver/SolverTest.cs
+++ UnitTests/Core/Solver/SolverTest.cs
@@ -1,7 +1,8 @@
+using System.Diagnostics;
 using Model.Core.Matrix;
 using Model.Core.Solver;
 using Model.Core.Solver.Precondition;
-using Xunit;
+using Model.Util;
 
 namespace UnitTests.Core.Solver;
 
@@ -24,13 +25,14 @@ public class PCGSolverTests
             { 1, 3 }
         });
 
-        var preconditioner = new IdentityPreconditioner();
-        var solver = new PCGSolver(matrix, preconditioner);
-
         double[] rhs = [6, 7];
         double[] solution = [0, 0];
 
-        solver.Solve(rhs, solution, new ISolver.SolverParams(1e-10, 1000));
+        var solver = new PCGSolver(m => new IdentityPreconditioner())
+        {
+            Matrix = matrix
+        };
+        solver.Solve(rhs, solution, new ISolver.Params(1e-10, 1000));
 
         AssertClose(1.0, solution[0]);
         AssertClose(2.0, solution[1]);
@@ -45,18 +47,20 @@ public class PCGSolverTests
             { 1, 3 }
         });
 
-        var preconditioner = new IdentityPreconditioner();
-        var solver = new PCGSolver(matrix, preconditioner);
 
         double[] rhs = [0, 0];
         double[] solution = [5, -3];
 
-        var result = solver.Solve(rhs, solution, new ISolver.SolverParams(1e-10, 1000));
+        var solver = new PCGSolver(m => new IdentityPreconditioner())
+        {
+            Matrix = matrix
+        };
+        var (residual, iterations) = solver.Solve(rhs, solution, new ISolver.Params(1e-10, 1000));
 
         AssertClose(0.0, solution[0]);
         AssertClose(0.0, solution[1]);
-        Assert.Equal(0, result.iterations);
-        AssertClose(0.0, result.residual);
+        Assert.Equal(0, iterations);
+        AssertClose(0.0, residual);
     }
 
     [Fact]
@@ -68,16 +72,18 @@ public class PCGSolverTests
             { 1, 3 }
         });
 
-        var preconditioner = new IdentityPreconditioner();
-        var solver = new PCGSolver(matrix, preconditioner);
 
         double[] rhs = [6, 7];
         double[] solution = [0, 0];
 
-        var result = solver.Solve(rhs, solution, new ISolver.SolverParams(1e-10, 1000));
+        var solver = new PCGSolver(_ => new IdentityPreconditioner())
+        {
+            Matrix = matrix
+        };
+        var (residual, iterations) = solver.Solve(rhs, solution, new ISolver.Params(1e-10, 1000));
 
-        Assert.True(result.residual < 1e-6,
-            $"Residual is too large: {result.residual}");
+        Assert.True(residual < 1e-6,
+            $"Residual is too large: {residual}");
     }
 
     [Fact]
@@ -89,13 +95,15 @@ public class PCGSolverTests
             { 1, 3 }
         });
 
-        var preconditioner = new IdentityPreconditioner();
-        var solver = new PCGSolver(matrix, preconditioner);
 
         double[] rhs = [6, 7];
         double[] solution = [0.9, 2.1];
 
-        solver.Solve(rhs, solution, new ISolver.SolverParams(1e-10, 1000));
+        var solver = new PCGSolver(_ => new IdentityPreconditioner())
+        {
+            Matrix = matrix
+        };
+        solver.Solve(rhs, solution, new ISolver.Params(1e-10, 1000));
 
         AssertClose(1.0, solution[0]);
         AssertClose(2.0, solution[1]);
@@ -110,16 +118,19 @@ public class PCGSolverTests
             { 1, 3 }
         });
 
-        var preconditioner = new IdentityPreconditioner();
-        var solver = new PCGSolver(matrix, preconditioner);
 
         double[] rhs = [6, 7];
         double[] solution = [0, 0];
 
-        var result = solver.Solve(rhs, solution, new ISolver.SolverParams(1e-30, 1));
+        var solver = new PCGSolver(_ => new IdentityPreconditioner())
+        {
+            Matrix = matrix
+        };
 
-        Assert.True(result.iterations <= 1,
-            $"Iterations: {result.iterations}");
+        var (residual, iterations) = solver.Solve(rhs, solution, new ISolver.Params(1e-30, 1));
+
+        Assert.True(iterations <= 1,
+            $"Iterations: {iterations}");
     }
 
     private class IdentityPreconditioner : IPreconditioner
@@ -131,14 +142,9 @@ public class PCGSolverTests
         }
     }
 
-    private class TestMatrix : IGlobalMatrix
+    private class TestMatrix(double[,] data) : IGlobalMatrix
     {
-        private readonly double[,] _data;
-
-        public TestMatrix(double[,] data)
-        {
-            _data = data;
-        }
+        private readonly double[,] _data = data;
 
         public int Size => _data.GetLength(0);
 
@@ -146,6 +152,8 @@ public class PCGSolverTests
 
         public void MulVec(ReadOnlySpan<double> vector, Span<double> result)
         {
+            Debug.Assert(vector.Length == Size);
+            Debug.Assert(result.Length == Size);
             for (int i = 0; i < Size; i++)
             {
                 double sum = 0;
@@ -154,6 +162,14 @@ public class PCGSolverTests
 
                 result[i] = sum;
             }
+        }
+
+        public object Clone() => new TestMatrix((double[,])_data.Clone());
+        public void Fill(double value) => _data.AsFlatSpan<double>().Fill(value);
+        public void Scale(double factor) {
+            var flatten = _data.AsFlatSpan<double>();
+            for (int i = 0; i < flatten.Length; ++i)
+                flatten[i] *= factor;
         }
     }
 }
--- UnitTests/Elements/Quadrangle/QTest1.cs
+++ UnitTests/Elements/Quadrangle/QTest1.cs
@@ -16,8 +16,8 @@ public class QTest1
     {
         var p1 = new Vector2D(0, 0);
         var p2 = new Vector2D(1, 0);
-        var p3 = new Vector2D(0, 1);
-        var p4 = new Vector2D(1, 1);
+        var p3 = new Vector2D(1, 1);
+        var p4 = new Vector2D(0, 1);
 
         var point = new QuadrangleCoordinateSystem(p1, p2, p3, p4);
         var p1_res = point.Transform(p1);
@@ -38,13 +38,13 @@ public class QTest1
         double eps = 1e-13;
         var tp1 = new Vector2D(0, 0);
         var tp2 = new Vector2D(1, 0);
-        var tp3 = new Vector2D(0, 1);
-        var tp4 = new Vector2D(1, 1);
+        var tp3 = new Vector2D(1, 1);
+        var tp4 = new Vector2D(0, 1);
 
         var p1 = new Vector2D(1, 5);
         var p2 = new Vector2D(6, 1);
-        var p3 = new Vector2D(5, 6);
-        var p4 = new Vector2D(7, 3);
+        var p3 = new Vector2D(7, 3);
+        var p4 = new Vector2D(5, 6);
 
         var point = new QuadrangleCoordinateSystem(p1, p2, p3, p4);
         var p1_res = point.Transform(p1);
