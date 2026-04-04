5f6e0e0a5d0248167272b0c70e9b173bd281f554
Creray <lebed.yaroslav.edu@gmail.com>
2026-04-04 12:25:52 +0700

    fix: (!) Use segment parametrization [-1; 1] instead of [0; 1]
    Фикс обоснован тем, что для квадратур используется параметризация [-1; 1]

--- Model/Core/CoordinateSystem/SegmentParametrization.cs
+++ Model/Core/CoordinateSystem/SegmentParametrization.cs
@@ -6,10 +6,10 @@ namespace Model.Core.CoordinateSystem;
 
 
 /// <summary>
-/// Parametrization: γ(t) = A + V * t
+/// Parametrization: γ(t) = A + V * t  (t∈[-1; 1])
 /// </summary>
 /// <typeparam name="TSource">Source vector dimensions</typeparam>
-/// <param name="a">Start point of the segment (t = 0)</param>
+/// <param name="a">Start point of the segment (t = -1)</param>
 /// <param name="b">End point of the segment (t = 1)</param>
 public sealed class SegmentParametrization<TSource>(TSource a, TSource b) :
     ICoordinateTransform<TSource, Vector1D>
@@ -22,12 +22,18 @@ public sealed class SegmentParametrization<TSource>(TSource a, TSource b) :
 
 
     public Vector1D Transform(TSource sourcePoint)
-        => ((sourcePoint - Origin) * Offset) / Offset.NormSqr;
+    {
+        var projection = ((sourcePoint - Origin) * Offset) / Offset.NormSqr;
+        return 2 * projection - 1;
+    }
 
     public TSource InverseTransform(Vector1D targetPoint)
-        => Origin + Offset * targetPoint;
+    {
+        double t = 0.5 * (targetPoint + 1);
+        return Origin + Offset * t;
+    }
 
-    public double Jacobian(Vector1D targetPoint) => 1.0 / Offset.Norm;
+    public double Jacobian(Vector1D targetPoint) => 2.0 / Offset.Norm;
 
     public IJacobyMatrix<Vector1D, TSource> InverseJacoby()
         => _j;
@@ -46,9 +52,9 @@ public sealed record InverseSegmentJacobyMatrix<TSource>(
         {
             Debug.Assert(0 <= i && i < IJacobyMatrix<Vector1D, TSource>.Rows);
             Debug.Assert(0 <= j && j < IJacobyMatrix<Vector1D, TSource>.Columns);
-            return Offset.AsSpan()[j];
+            return Offset.AsSpan()[j] / 2.0;
         }
     }
     public double this[int i, int j, Vector1D _] => this[i, j];
-    public double Det(Vector1D _) => Offset.Norm;
+    public double Det(Vector1D _) => Offset.Norm / 2.0;
 }
