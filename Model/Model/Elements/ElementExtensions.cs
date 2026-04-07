7b6c24e4dde20f4995b2af4a1b37c2a54a6daec3
Creray <lebed.yaroslav.edu@gmail.com>
2026-04-07 21:43:23 +0700

    feat: Added finite element `Evaluate` extension method

--- Model/Model/Elements/ElementExtensions.cs
+++ Model/Model/Elements/ElementExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Model.Core.CoordinateSystem;
 using Model.Model.Basis;
 using Model.Model.Mesh;
@@ -13,10 +14,42 @@ public static class FiniteElementExtensions
         where TSpace : IVectorBase<TSpace>
         where TBoundary : IVectorBase<TBoundary>
     {
+        /// <summary>
+        /// Evaluates mesh function value at given point inside element
+        /// </summary>
+        /// <param name="point">Point defined in mesh-space coordinates</param>
+        /// <param name="basisCoefficients">Basis function coefficients indexed by global dof indices (q array)</param>
+        public double Evaluate(ReadOnlySpan<double> basisCoefficients, TSpace point)
+        {
+            Debug.Assert(self.ContainsPoint(point, epsilon: 1e-12));
+            var ep = self.MasterElementCoordinateSystem.Transform(point); // element-space point
+
+            double value = 0.0;
+            for (int i = 0; i < self.DOF.Count; ++i)
+            {
+                double q = basisCoefficients[self.DOF.Dof[i]];
+                value += q * self.Basis[i].Value(ep);
+            }
+
+            return value;
+        }
+    }
+}
+
+/// <summary>
+/// Utility methods to inline finite element member calls
+/// </summary>
+public static class FiniteElementComposition
+{
+    extension<TSpace, TBoundary>(IFiniteElementBase<TSpace, TBoundary> self)
+        where TSpace : IVectorBase<TSpace>
+        where TBoundary : IVectorBase<TBoundary>
+    {
         public IMesh<TSpace> Mesh => self.Geometry.Mesh;
         public ReadOnlySpan<int> Vertices => self.Geometry.Vertices;
         public IEnumerable<Edge> Edges => self.Geometry.Edges;
         public int EdgeCount => self.Geometry.EdgeCount;
+        public bool ContainsPoint(TSpace point, double epsilon = 1e-12) => self.Geometry.ContainsPoint(point, epsilon);
         public ICoordinateTransform<TSpace, TBoundary> MasterElementCoordinateSystem => self.Geometry.MasterElementCoordinateSystem;
 
         public IEnumerable<Quadratures.Node<TBoundary>> Quadratures => self.BasisSet.Quadratures;
