5d50e408feb939a999578b7fe4de338d79bb9441
Creray <lebed.yaroslav.edu@gmail.com>
2026-04-10 21:23:53 +0700

    refactor: Use renumeration instead of fn swapping

--- Model/Model/Elements/Segment/LagrangeCubic.cs
+++ Model/Model/Elements/Segment/LagrangeCubic.cs
@@ -9,16 +9,14 @@ public sealed class LagrangeCubicSegmentFactory : IBoundaryElementFactory2D
 {
     public IBoundaryElement2D CreateBoundary(IMesh2D mesh, int[] vertices, int boundaryIndex)
     {
-        var basis = (MutableBasisSet<Vector1D>)DefaultBasis();
         return new BoundaryElement2D(
             Geometry: new SegmentGeometry<Vector2D>(vertices) { Mesh = mesh },
-            DOF: new Dof(basis),
-            BasisSet: basis,
+            DOF: new Dof(),
+            BasisSet: Basis,
             BoundaryIndex: boundaryIndex);
     }
 
-
-    public static IBasisSet1D DefaultBasis() => new MutableBasisSet<Vector1D>(
+    public static IBasisSet1D Basis => new BasisSet1D(
         Quadratures.SegmentGaussOrder7,
         SegmentBasis.Lagrange1D.Create(3, 0),
         SegmentBasis.Lagrange1D.Create(3, 3),
@@ -26,9 +24,8 @@ public sealed class LagrangeCubicSegmentFactory : IBoundaryElementFactory2D
         SegmentBasis.Lagrange1D.Create(3, 2)
     );
 
-    public sealed class Dof(MutableBasisSet<Vector1D> mutableBasis) : DofManager(dofCount: 4)
+    public sealed class Dof() : DofManager(dofCount: 4)
     {
-        private readonly MutableBasisSet<Vector1D> _mutableBasisSet = mutableBasis;
         public override int NumberOfDofOnVertex => 1;
         public override int NumberOfDofOnEdge => 2;
         public override int NumberOfDofOnElement => 0;
@@ -43,14 +40,9 @@ public sealed class LagrangeCubicSegmentFactory : IBoundaryElementFactory2D
         public override void SetEdgeDof(int localEdgeIndex, bool isOrientationFlipped, int n, int dofIndex)
         {
             AssertIsValidEdgeDofNumber(n);
-            Debug.Assert(0 == localEdgeIndex);
-            _dof[n + 2] = dofIndex;
-
-            if (isOrientationFlipped)
-            {
-                _mutableBasisSet.MutableBasis[2] = SegmentBasis.Lagrange1D.Create(3, 2);
-                _mutableBasisSet.MutableBasis[3] = SegmentBasis.Lagrange1D.Create(3, 1);
-            }
+            Debug.Assert(localEdgeIndex == 0);
+            if (isOrientationFlipped) n = 1 - n;
+            _dof[2 + n] = dofIndex;
         }
 
         public override void SetElementDof(int n, int dofIndex) =>
