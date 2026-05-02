238c472c2b39771ae2dd7db2216617d1c2a17ecd
Михаил Рояк <mikeroyak@gmail.com>
2026-05-02 21:06:55 +0700

    Пример графического окна на Авалонии

--- View/MainWindow.axaml.cs
+++ View/MainWindow.axaml.cs
@@ -1,10 +1,12 @@
+using System.Globalization;
 using System.Linq;
+using App.ViewModels;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Avalonia.Platform.Storage;
-using App.ViewModels;
 
 namespace App
 {
@@ -36,6 +38,43 @@ namespace App
             vm.MeshFilePath = file.Path.LocalPath;
             vm.LoadGeometryCommand.Execute().Subscribe();
         }
+        public static FormattedText ConvertToAvaloniaText(string text)
+        {
+            return new FormattedText(
+                text,
+                CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
+                new Typeface("Times"), 20,
+                null)
+            {
+                TextAlignment = TextAlignment.Left,                
+            };
+        }
 
+        private void Button_Click(object? sender, RoutedEventArgs e)
+        {
+            var gdrawing = new DrawingGroup();
+
+
+            gdrawing.Children.Add(new GeometryDrawing()
+            {
+                Brush = Brushes.Red,
+                Pen = new Pen(Colors.Blue.ToUInt32()),
+                Geometry = new EllipseGeometry(new Rect(100, 100, 300, 200))
+                
+            });
+            var text = ConvertToAvaloniaText("Hello from Avalonia");
+            text.SetForegroundBrush(Brushes.Green);
+            gdrawing.Children.Add(new GeometryDrawing()
+            {
+                Brush = Brushes.Black,
+                Pen = new Pen(Colors.Blue.ToUInt32()),
+                Geometry = text.BuildGeometry(new Point(200,200))                        
+            });
+
+
+
+            DrawHost.Source = new DrawingImage(gdrawing);
+
+        }
     }
 }
